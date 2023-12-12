e5d5d8c90fa246d7f28d6dfcfdd5edfa7e696923
Lucas barros <fallenagamesytb@gmail.com>
2023-12-11 21:32:31 -0300

    Criando agenda

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form-contato");
+const nome = document.getElementById("nome-pessoa");
+const numeroTel = document.getElementById("numero-pessoa");
+
+let linhas = '';
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    let linha = '<tr>';
+    linha += `<td>${nome.value}</td>`;
+    linha += `<td>${numeroTel.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    nome.value = '';
+    numeroTel.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector("tbody");
+    corpoTabela.innerHTML = linhas;
+}
+
